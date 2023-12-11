23dbb84f847fdfd77f38544b2b466506e9dd34b5
Juliet <musyoki.juliet011@gmail.com>
2023-12-11 13:21:03 +0300

    DONE

--- lib/models.py
+++ lib/models.py
@@ -15,6 +15,28 @@ class Customer(Base):
     # Define relationships
     reviews = relationship("Review", back_populates="customer")
 
+    def reviews(self):
+        return self.reviews
+
+    def restaurants(self):
+        return [review.restaurant for review in self.reviews]
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def favorite_restaurant(self):
+        if not self.reviews:
+            return None
+        return max(self.reviews, key=lambda review: review.star_rating).restaurant
+
+    def add_review(self, restaurant, rating):
+        review = Review(star_rating=rating, customer=self, restaurant=restaurant)
+        self.reviews.append(review)
+        return review
+
+    def delete_reviews(self, restaurant):
+        self.reviews = [review for review in self.reviews if review.restaurant != restaurant]
+
 class Restaurant(Base):
     __tablename__ = "restaurants"
 
@@ -24,6 +46,21 @@ class Restaurant(Base):
 
     # Define relationships
     reviews = relationship("Review", back_populates="restaurant")
+
+    def reviews(self):
+        return self.reviews
+
+    def customers(self):
+        return [review.customer for review in self.reviews]
+
+    @classmethod
+    def fanciest(cls):
+        if not cls.query.all():
+            return None
+        return max(cls.query.all(), key=lambda restaurant: restaurant.price)
+
+    def all_reviews(self):
+        return [f"Review for {self.name} by {review.customer.full_name()}: {review.star_rating} stars." for review in self.reviews]
 
 class Review(Base):
     __tablename__ = "reviews"
@@ -36,3 +73,12 @@ class Review(Base):
     # Define relationships
     customer = relationship("Customer", back_populates="reviews")
     restaurant = relationship("Restaurant", back_populates="reviews")
+
+    def customer(self):
+        return self.customer
+
+    def restaurant(self):
+        return self.restaurant
+
+    def full_review(self):
+        return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars."
