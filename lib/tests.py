dc5cfb29e921239cd133254c5277f99ed1253137
Juliet <musyoki.juliet011@gmail.com>
2023-12-11 14:10:07 +0300

    DONE

--- lib/tests.py
+++ lib/tests.py
@@ -37,5 +37,25 @@ class TestRestaurantReviews(unittest.TestCase):
         retrieved_review = self.db.query(Review).filter_by(star_rating=4).first()
         self.assertEqual(retrieved_review.customer.first_name, "John")
 
+    def test_customer_full_name(self):
+        customer = Customer(first_name="John", last_name="Doe")
+        self.db.add(customer)
+        self.db.commit()
+        retrieved_customer = self.db.query(Customer).filter_by(first_name="John").first()
+        self.assertEqual(retrieved_customer.full_name(), "John Doe")
+
+    def test_customer_favorite_restaurant(self):
+        customer = Customer(first_name="John", last_name="Doe")
+        restaurant1 = Restaurant(name="Tasty Bites", price=2)
+        restaurant2 = Restaurant(name="Gourmet Grill", price=3)
+        review1 = Review(star_rating=4, customer=customer, restaurant=restaurant1)
+        review2 = Review(star_rating=5, customer=customer, restaurant=restaurant2)
+        self.db.add_all([customer, restaurant1, restaurant2, review1, review2])
+        self.db.commit()
+        retrieved_customer = self.db.query(Customer).filter_by(first_name="John").first()
+        self.assertEqual(retrieved_customer.favorite_restaurant(), restaurant2)
+
+    # Add more tests for other methods in the Customer model
+
 if __name__ == '__main__':
     unittest.main()
